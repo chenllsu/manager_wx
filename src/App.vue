f9d4c77d2364c4b8190af347a20fb07c4b1527e0
liyan <609505217@qq.com>
2017-06-01 09:57:33 +0800

    flex template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="background">
+    <div class="title-wrapper">
+      我是title
+    </div>
+    <div class="body-wrapper">
+      <div class="menu-wrapper">
+        <div class="menu-button-wrapper" @click="clickbutton1">
+          1
+        </div>
+        <div class="menu-button-wrapper" @click="clickbutton2">
+          2
+        </div>
+      </div>
+      <div class="content-wrapper">
+        <div v-if="button1 === true">
+          <router-link to="/button1">{{button1}}</router-link>
+        </div>
+        <div v-if="button2 === true">
+          <router-link to="/button2"></router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data() {
+    return {
+      button1: false,
+      button2: false
+    }
+  },
+  methods: {
+    clickbutton1() {
+      this.button1 = !this.button1;
+    },
+    clickbutton2() {
+      this.button2 = !this.button2;
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+.background
+  position: absolute
+  height: 100%
+  width: 100%
+  display: flex
+  flex-direction: column
+  background: #fff
+  .title-wrapper
+    position: relative
+    flex: 0 0 100px
+    background: #f3f5f7
+  .body-wrapper
+    flex: 1
+    display: flex
+    .menu-wrapper
+      flex: 0 0 200px
+    .content-wrapper
+      flex: 1
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import button1 from './components/button1'
+import button2 from './components/button2'
 
 Vue.config.productionTip = false
+Vue.use(router);
+
+const routes = [{
+  path: '/button1',
+  component: button1
+}, {
+  path: '/button2',
+  component: button2
+}];
 
 /* eslint-disable no-new */
 new Vue({
